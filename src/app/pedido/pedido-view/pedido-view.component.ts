25277396754e3ebdc72822861674fc129e8bdcb8
Andrade <igor.andrade@capgemini.com>
2020-12-11 07:22:44 -0300

    add agGrid

--- src/app/pedido/pedido-view/pedido-view.component.ts
+++ src/app/pedido/pedido-view/pedido-view.component.ts
@@ -10,7 +10,15 @@ export class PedidoViewComponent implements OnInit {
 
   pedido:Pedido = {id: 0, cliente: '', produto: '', freteTarifado: false, data: ''};
 
-  pedidos: Array<Pedido> = [
+  columnDefs = [
+    { field: 'id', sortable: true, filter: true },
+    { field: 'cliente', sortable: true, filter: true  },
+    { field: 'produto', sortable: true, filter: true },
+    { field: 'freteTarifado', sortable: true, filter: true },
+    { field: 'data', sortable: true, filter: true }
+  ];
+
+  rowData : Array<Pedido> = [
     {id: 1, cliente: 'Igor', produto: 'laptop', freteTarifado: false, data: '01/01/2021'},
     {id: 1, cliente: 'Ana', produto: 'laptop', freteTarifado: true, data: '01/01/2021'},
     {id: 1, cliente: 'Maria', produto: 'laptop', freteTarifado: false, data: '01/01/2021'},
